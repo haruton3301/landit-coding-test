892a45a10c65da647ab7a5802ca4186b4b2c250b
Haruto Ikehara <kduxwr@gmail.com>
2025-10-17 20:43:03 +0900

    コントローラを修正

--- src/controllers/estate-transaction.controller.ts
+++ src/controllers/estate-transaction.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { EstateTransactionQueryDto } from './dto/estate-transaction-query.dto';
+import { GetEstateTransactionBarUseCase } from '../use-cases/get-estate-transaction-bar.usecase';
 
 @Controller('api/v1/townPlanning/estateTransaction')
 export class EstateTransactionController {
+  constructor(
+    private readonly getEstateTransactionBarUseCase: GetEstateTransactionBarUseCase,
+  ) {}
+
   @Get('bar')
   async getBar(@Query() query: EstateTransactionQueryDto) {
-    return {
-      message: 'dummy',
-      query,
-    };
+    return await this.getEstateTransactionBarUseCase.execute({
+      year: query.year,
+      prefectureCode: query.prefectureCode,
+      type: query.type,
+    });
   }
 }
