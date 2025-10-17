a42b6064103e9edd534525f1d8021a72dc922f19
Haruto Ikehara <kduxwr@gmail.com>
2025-10-17 20:43:10 +0900

    DIの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EstateTransactionController } from './controllers/estate-transaction.controller';
+import { GetEstateTransactionBarUseCase } from './use-cases/get-estate-transaction-bar.usecase';
+import { JsonEstateTransactionRepository } from './infrastructure/json-estate-transaction.repository';
 
 @Module({
   imports: [],
   controllers: [EstateTransactionController],
-  providers: [],
+  providers: [
+    GetEstateTransactionBarUseCase,
+    {
+      provide: 'IEstateTransactionRepository',
+      useClass: JsonEstateTransactionRepository,
+    },
+  ],
 })
 export class AppModule {}
